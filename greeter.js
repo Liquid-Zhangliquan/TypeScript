7d68f3f9be213d8eeda6ce47db9b1eee3a56ab78
Liquid-Zhangliquan <1063754495@qq.com>
2018-12-25 16:31:09 +0800

    add enum

--- greeter.js
+++ greeter.js
@@ -17,6 +17,42 @@ var hexLiteral = 0xf00d;
 var pname = "bob";
 pname = 'zhangliquan';
 var age = 24;
-var sentence = "Hello, my name is " + pname + ".\n\nI'll be " + (age + 1) + " years old next month.";
+var sentence = "Hello, my name is " + pname + ".\nI'll be " + (age + 1) + " years old next month.";
+var list = [1, 2, 3];
+var list1 = [1, 2, 3];
+//元组 Tuple元组类型允许表示一个已知元素数量和类型的数组，各元素的类型不必相同。 比如，你可以定义一对值分别为 string和number类型的元组。
+var x;
+x = ['hello', 10];
+//枚举enum类型是对JavaScript标准数据类型的一个补充。 像C#等其它语言一样，使用枚举类型可以为一组数值赋予友好的名字。
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+;
+var c = Color.green;
+//默认情况下，从0开始为元素编号。你也可以手动的指定成员的数值。 例如，我们将上面的例子改成从 1开始编号：
+var Color1;
+(function (Color1) {
+    Color1["red"] = "red";
+    Color1["blue"] = "blue";
+    Color1["green"] = "green";
+})(Color1 || (Color1 = {}));
+var c1 = Color1.blue;
+var Color2;
+(function (Color2) {
+    Color2[Color2["Red"] = 1] = "Red";
+    Color2[Color2["Green"] = 2] = "Green";
+    Color2[Color2["Blue"] = 3] = "Blue";
+})(Color2 || (Color2 = {}));
+var c2 = Color2.Green;
+var Color3;
+(function (Color3) {
+    Color3[Color3["Red"] = 1] = "Red";
+    Color3[Color3["Green"] = 2] = "Green";
+    Color3[Color3["Blue"] = 3] = "Blue";
+})(Color3 || (Color3 = {}));
+var colorName = Color3[2];
 //document.body.innerHTML = greeter(user);
 document.body.innerHTML = sentence;
--- greeter.ts
+++ greeter.ts
@@ -10,21 +10,42 @@ interface Person {
     lastName: string;
 }
 
-function greeter(person : Person) {
+function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
 let user = new Student("Jane", "M.", "User");
 
-let isDone:boolean=false;
-let decLiteral:number=6;
-let hexLiteral:number=0xf00d;
-let pname:string="bob";
-pname='zhangliquan';
-let age:number=24;
-let sentence:string=`Hello, my name is ${ pname }.
-I'll be ${ age + 1 } years old next month.`
-let list:number[]=[1,2,3];
-let list1:Array<number>=[1,2,3];
+let isDone: boolean = false;
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let pname: string = "bob";
+pname = 'zhangliquan';
+let age: number = 24;
+let sentence: string = `Hello, my name is ${pname}.
+I'll be ${ age + 1} years old next month.`
+let list: number[] = [1, 2, 3];
+let list1: Array<number> = [1, 2, 3];
+//元组 Tuple元组类型允许表示一个已知元素数量和类型的数组，各元素的类型不必相同。 比如，你可以定义一对值分别为 string和number类型的元组。
+let x: [string, number];
+x = ['hello', 10]
+//枚举enum类型是对JavaScript标准数据类型的一个补充。 像C#等其它语言一样，使用枚举类型可以为一组数值赋予友好的名字。
+enum Color { red, green, blue };
+let c: Color = Color.green;
+//默认情况下，从0开始为元素编号。你也可以手动的指定成员的数值。 例如，我们将上面的例子改成从 1开始编号：
+enum Color1 { red = 'red', blue = 'blue', green = 'green' }
+let c1: string = Color1.blue;
+
+enum Color2 { Red = 1, Green, Blue }
+let c2: number = Color2.Green;
+
+enum Color3 { Red1 = 1, Green1, Blue1 }
+let colorName: string = Color[2];
+
+
+
+
+
+
 //document.body.innerHTML = greeter(user);
 document.body.innerHTML = sentence;
