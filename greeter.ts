89acc46aaae553209bc8bad0ce1c514b87af03b2
Liquid-Zhangliquan <1063754495@qq.com>
2019-01-04 17:26:45 +0800

    void

--- greeter.ts
+++ greeter.ts
@@ -42,6 +42,19 @@ let c2: number = Color2.Green;
 enum Color3 { Red1 = 1, Green1, Blue1 }
 let colorName: string = Color[2];
 
+//某种程度上来说，void类型像是与any类型相反，它表示没有任何类型。 当一个函数没有返回值时，你通常会见到其返回值类型是 void：
+//声明一个void类型的变量没有什么大用，因为你只能为它赋予undefined和null：
+function warnuser():void{
+    console.log("this is my warning message");
+}
+
+//类型断言
+//有时候你会遇到这样的情况，你会比TypeScript更了解某个值的详细信息。 通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型。
+//通过类型断言这种方式可以告诉编译器，“相信我，我知道自己在干什么”。 类型断言好比其它语言里的类型转换，但是不进行特殊的数据检查和解构。 它没有运行时的影响，只是在编译阶段起作用。 TypeScript会假设你，程序员，已经进行了必须的检查。
+let somevalue:any="this is a string";
+let strlength:number=(<string>somevalue).length;
+let _strlength:number=(somevalue as string).length;
+
 
 
 
